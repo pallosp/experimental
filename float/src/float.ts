75d3b91f64386ae5b37b5272035fbe6fc7dc70c4
Péter Pallos <pallosp@gmail.com>
2024-12-19 21:40:21 +0100

    Add prevDouble

--- float/src/float.ts
+++ float/src/float.ts
@@ -1,7 +1,7 @@
 /** Largest float64 that is less than 1 */
 const BEFORE_1 = 1 - 1e-16;
 
-/** Returns the smallest possible float64 that is greater than x. */
+/** Returns the lowest possible float64 that is greater than x. */
 export function nextDouble(x: number): number {
   if (isFinite(x)) {
     return x > 0 ? x / (Math.min(1 - Number.MIN_VALUE / x / 1.8, BEFORE_1)) :
@@ -10,3 +10,13 @@ export function nextDouble(x: number): number {
   }
   return x < 0 ? -Number.MAX_VALUE : NaN;
 };
+
+/** Returns the greatest possible float64 that is less than x. */
+export function prevDouble(x: number): number {
+  if (isFinite(x)) {
+    return x > 0 ? x * (Math.min(1 - Number.MIN_VALUE / x / 1.8, BEFORE_1)) :
+        x < 0    ? x / (Math.min(1 - Number.MIN_VALUE / x / -1.8, BEFORE_1)) :
+                   -Number.MIN_VALUE;
+  }
+  return x > 0 ? Number.MAX_VALUE : NaN;
+}
