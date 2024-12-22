1bf4799acd04360cd46d073ed134583c1428e590
Péter Pallos <pallosp@gmail.com>
2024-12-22 14:20:37 +0100

    Implement and test splitting float64s

--- float/tests/float116.test.ts
+++ float/tests/float116.test.ts
@@ -1,8 +1,10 @@
 import {expect, test} from '@jest/globals';
 
-import {lsbExp} from '../src/float';
+import {lsbExp, msbExp} from '../src/float';
 
 import {randomInt, randomSign} from './random';
+
+const ATTEMPTS = 100;
 
 interface Float116 {
   hi: number;
@@ -14,8 +16,6 @@ function addDD(x: number, y: number): Float116 {
   const x1 = sum - y;
   return {hi: sum, lo: x - x1 + (x1 - sum + y)};
 }
-
-const ATTEMPTS = 100;
 
 test('addDD', () => {
   for (let i = 0; i < ATTEMPTS; i++) {
@@ -33,5 +33,44 @@ test('addDD', () => {
       expect(Math.abs(sum.lo)).not.toBe(0);
       expect(lsbExp(sum.lo)).toBe(Math.min(lsb1, lsb2));
     }
+  }
+});
+
+// Splits a float64 to the sum of two float64s with non-overlapping
+// significands. Both parts have at most 26 significant bits.
+function splitD(x: number): Float116 {
+  const splitter = 0x7ffffff;
+  const hi = x + x * splitter - x * splitter;
+  return {hi, lo: x - hi};
+}
+
+// Checks whether splitD does the right thing for the given number:
+// - The sum must match.
+// - The parts must be disjoint.
+// - The parts must have few enough significant bits so that squaring them or
+//   multiplying them with each other won't cause loss of precision.
+function isWellSplit(x: number) {
+  const {hi, lo} = splitD(x);
+  return x === hi + lo &&  //
+      lsbExp(hi * hi) === lsbExp(hi) * 2 &&
+      lsbExp(lo * lo) === lsbExp(lo) * 2 &&
+      lsbExp(lo * hi) === lsbExp(lo) + lsbExp(hi) &&
+      (lo === 0 || lsbExp(hi) > msbExp(lo));
+}
+
+test('splitD', () => {
+  console.log(splitD(Number.MAX_SAFE_INTEGER));
+  expect(isWellSplit(0)).toBe(true);
+  for (const exp of [26, 27, 28, 52, 53]) {
+    expect(isWellSplit(2 ** exp - 1)).toBe(true);
+    expect(isWellSplit(2 ** exp)).toBe(true);
+    expect(isWellSplit(2 ** exp + 1)).toBe(true);
+  }
+  expect(isWellSplit(2 ** 53 + 2)).toBe(true);
+  expect(isWellSplit(0x1ffffff7ffffff)).toBe(true);
+
+  for (let i = 0; i < ATTEMPTS; i++) {
+    const x = randomInt(0, Number.MAX_SAFE_INTEGER);
+    expect(isWellSplit(x)).toBe(true);
   }
 });
