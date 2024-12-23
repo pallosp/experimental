c18ac37304724a630611b4e38d5c34b3613f36f1
Péter Pallos <pallosp@gmail.com>
2024-12-23 16:46:10 +0100

    Move sumLowerBound and sumUpperBound to sum.ts

--- float/src/sum.ts
+++ float/src/sum.ts
@@ -1,5 +1,21 @@
+import {nextDouble, prevDouble} from './enumerate';
+
 /** Whether x+y can be exactly represented as a float64. */
 export function isSumExact(x: number, y: number): boolean {
   const sum = x + y;
   return sum - x === y && sum - y === x;
+}
+
+/** Returns the greatest possible float64 that is at most x+y. */
+export function sumLowerBound(x: number, y: number): number {
+  const sum = x + y;
+  if (!isFinite(x) || !isFinite(y)) return sum;
+  return sum - x <= y && sum - y <= x ? sum : prevDouble(sum);
+}
+
+/** Returns the lowest possible float64 that is at least x+y. */
+export function sumUpperBound(x: number, y: number): number {
+  const sum = x + y;
+  if (!isFinite(x) || !isFinite(y)) return sum;
+  return sum - x >= y && sum - y >= x ? sum : nextDouble(sum);
 }
--- float/tests/float.test.ts
+++ float/tests/float.test.ts
@@ -1,85 +1,10 @@
 import {expect, test} from '@jest/globals';
 
-import {lsbExp} from '../src/bits';
-import {nextDouble, prevDouble} from '../src/enumerate';
-import {mulLowerBound, mulUpperBound, sumLowerBound, sumUpperBound} from '../src/float';
+import {mulLowerBound, mulUpperBound} from '../src/float';
 
-import {randomInt, randomSign} from './random';
+import {randomSign} from './random';
 
 const ATTEMPTS = 100;
-
-test('additive error tracking is consistent', () => {
-  for (let i = 0; i < ATTEMPTS; i++) {
-    const a = 1 / Math.random() * randomSign();
-    const b = 1 / Math.random() * randomSign();
-    const sum = a + b;
-    expect((sum - a - b) * (sum - b - a)).toBeGreaterThanOrEqual(0);
-  }
-});
-
-test('sum, lower/upper bounds, non-finite', () => {
-  expect(sumLowerBound(Infinity, 1)).toBe(Infinity);
-  expect(sumUpperBound(Infinity, 1)).toBe(Infinity);
-  expect(sumLowerBound(Infinity, -Infinity)).toBe(NaN);
-  expect(sumUpperBound(Infinity, -Infinity)).toBe(NaN);
-  expect(sumLowerBound(3, NaN)).toBe(NaN);
-  expect(sumUpperBound(3, NaN)).toBe(NaN);
-});
-
-test('sum, lower/upper bounds, exact result', () => {
-  expect(sumLowerBound(1, 2)).toBe(3);
-  expect(sumUpperBound(1, 2)).toBe(3);
-  expect(sumLowerBound(0.25, 0.0625)).toBe(0.3125);
-  expect(sumUpperBound(0.25, 0.0625)).toBe(0.3125);
-});
-
-test('sum, lower/upper bounds, non-exact finite result', () => {
-  expect(sumLowerBound(0.1, 0.2)).toBe(0.3);
-  expect(sumUpperBound(0.1, 0.2)).toBe(nextDouble(0.3));
-  expect(sumLowerBound(4.2, 1e-20)).toBe(4.2);
-  expect(sumUpperBound(4.2, 1e-20)).toBe(nextDouble(4.2));
-  expect(sumLowerBound(4.2, -1e-20)).toBe(prevDouble(4.2));
-  expect(sumUpperBound(4.2, -1e-20)).toBe(4.2);
-  expect(sumLowerBound(1e-20, 4.2)).toBe(4.2);
-  expect(sumUpperBound(1e-20, 4.2)).toBe(nextDouble(4.2));
-  expect(sumLowerBound(-1e-20, 4.2)).toBe(prevDouble(4.2));
-  expect(sumUpperBound(-1e-20, 4.2)).toBe(4.2);
-  expect(sumLowerBound(Number.MAX_SAFE_INTEGER, 2))
-      .toBe(Number.MAX_SAFE_INTEGER + 1);
-  expect(sumUpperBound(Number.MAX_SAFE_INTEGER, 2))
-      .toBe(Number.MAX_SAFE_INTEGER + 3);
-});
-
-test('sum, lower/upper bounds, near ∞', () => {
-  expect(sumLowerBound(Number.MAX_VALUE, Number.MAX_VALUE))
-      .toBe(Number.MAX_VALUE);
-  expect(sumUpperBound(Number.MAX_VALUE, Number.MAX_VALUE)).toBe(Infinity);
-  expect(sumLowerBound(Number.MAX_VALUE, -Number.MAX_VALUE)).toBe(0);
-  expect(sumUpperBound(Number.MAX_VALUE, -Number.MAX_VALUE)).toBe(0);
-  expect(sumLowerBound(-Number.MAX_VALUE, -Number.MAX_VALUE)).toBe(-Infinity);
-  expect(sumUpperBound(-Number.MAX_VALUE, -Number.MAX_VALUE))
-      .toBe(-Number.MAX_VALUE);
-});
-
-test('sum, lower/upper bounds, commutative', () => {
-  for (let i = 0; i < ATTEMPTS; i++) {
-    const a = randomInt(0, 99) / 100;
-    const b = randomInt(0, 99) / 100;
-    expect(sumLowerBound(a, b)).toBe(sumLowerBound(b, a));
-    expect(sumUpperBound(a, b)).toBe(sumUpperBound(b, a));
-  }
-});
-
-test('sum, lower/upper bounds, exactness', () => {
-  for (let i = 0; i < ATTEMPTS; i++) {
-    const a = 1 / Math.random() * randomSign();
-    const b = 1 / Math.random() * randomSign();
-    const lsb1 = lsbExp(a);
-    const lsb2 = lsbExp(b);
-    const isSumExact = lsb1 === lsb2 || lsbExp(a + b) === Math.min(lsb1, lsb2);
-    expect(sumLowerBound(a, b) === sumUpperBound(a, b)).toBe(isSumExact);
-  }
-});
 
 test('multiplicative error tracking is consistent', () => {
   for (let i = 0; i < ATTEMPTS; i++) {
