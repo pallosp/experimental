7ea1838a3dc72c7c42bf0339d4fe7d44b7bde8e8
Péter Pallos <pallosp@gmail.com>
2024-12-24 14:02:56 +0100

    Simple but slow, BigInt-based implementation of exact float64 multiplication

--- float/tests/product.test.ts
+++ float/tests/product.test.ts
@@ -1,6 +1,44 @@
 import {expect, test} from '@jest/globals';
 
+import {lsbExp} from '../src/bits';
+import {Float116} from '../src/float116';
 import {isProductExact} from '../src/product';
+import {addDD} from '../src/sum';
+
+/** Simple but slow implementation of exact float64 multiplication. */
+function mulDDSlow(x: number, y: number): Float116 {
+  const lsbX = lsbExp(x);
+  const lsbY = lsbExp(y);
+  if (!isFinite(lsbX) || !isFinite(lsbY)) return {hi: x * y, lo: 0};
+  const p = BigInt(x / 2 ** lsbX) * BigInt(y / 2 ** lsbY);
+  const lo = p % BigInt(2 ** 53);
+  const m = 2 ** (lsbX + lsbY);
+  return addDD(Number(p - lo) * m, Number(lo) * m);
+}
+
+test('mulDDSlow', () => {
+  // zero factor
+  expect(mulDDSlow(0, 12)).toEqual({hi: 0, lo: 0});
+  // fractional numbers
+  expect(mulDDSlow(1.25, 6.0625)).toEqual({hi: 1.25 * 6.0625, lo: 0});
+  // small * large integer
+  expect(mulDDSlow(Number.MAX_SAFE_INTEGER, 3))
+      .toEqual({hi: 3 * 2 ** 53 - 4, lo: 1});
+  // large * large integer
+  expect(mulDDSlow(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER))
+      .toEqual({hi: 2 ** 106 - 2 ** 54, lo: 1});
+  // subnormal product
+  expect(mulDDSlow(6 * Number.MIN_VALUE, 7))
+      .toEqual({hi: 42 * Number.MIN_VALUE, lo: 0});
+  // near MAX_VALUE
+  expect(mulDDSlow(Number.MAX_VALUE, 0.75))
+      .toEqual({hi: 3 * 2 ** 1022 - 4 * 2 ** 969, lo: 2 ** 969});
+  // non-finite factors
+  expect(mulDDSlow(Infinity, 0)).toEqual({hi: NaN, lo: 0});
+  expect(mulDDSlow(Infinity, 0.5)).toEqual({hi: Infinity, lo: 0});
+  expect(mulDDSlow(Infinity, -Infinity)).toEqual({hi: -Infinity, lo: 0});
+  expect(mulDDSlow(1, NaN)).toEqual({hi: NaN, lo: 0});
+});
 
 test('isProductExact', () => {
   expect(isProductExact(2, 3)).toBe(true);
