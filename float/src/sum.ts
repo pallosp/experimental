3667534b606c779f5ed5d203c33ec85081a6eb65
Péter Pallos <pallosp@gmail.com>
2024-12-25 20:57:22 +0100

    Avoid expensive function calls inside sum.ts

--- float/src/sum.ts
+++ float/src/sum.ts
@@ -10,26 +10,26 @@ export function isSumExact(x: number, y: number): boolean {
 /** Returns the greatest possible float64 that is at most x+y. */
 export function sumLowerBound(x: number, y: number): number {
   const sum = x + y;
-  if (!isFinite(x) || !isFinite(y)) return sum;
+  if (x - x !== 0 || y - y !== 0) return sum;
   return sum - x <= y && sum - y <= x ? sum : prevDouble(sum);
 }
 
 /** Returns the lowest possible float64 that is at least x+y. */
 export function sumUpperBound(x: number, y: number): number {
   const sum = x + y;
-  if (!isFinite(x) || !isFinite(y)) return sum;
+  if (x - x !== 0 || y - y !== 0) return sum;
   return sum - x >= y && sum - y >= x ? sum : nextDouble(sum);
 }
 
 /** Floating point rounding error of x+y. */
 export function errorOfSum(x: number, y: number): number {
   const sum = x + y;
-  if (!isFinite(sum)) return isFinite(x - y) ? sum : 0;
+  if (sum - sum !== 0) return x - x === 0 && y - y === 0 ? sum : 0;
   const x1 = sum - y;
   return x1 - x - (x1 - sum + y);
 }
 
-/** Calculates x+y with full of precision. */
+/** Calculates x+y with full precision. */
 export function addDD(x: number, y: number): Float116 {
   return {hi: x + y, lo: 0 - errorOfSum(x, y)};
 }
