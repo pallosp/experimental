5e2f097c8f1c94a802019c077f03438015298d51
Péter Pallos <pallosp@gmail.com>
2025-02-27 15:41:22 +0100

    Update contour_plot_example

--- contour_plot_example/index.ts
+++ contour_plot_example/index.ts
@@ -1,28 +1,19 @@
-import {Plot, squaresToSvg} from 'contour-plot-svg';
+import {Plot, runsToSvg} from 'contour-plot-svg';
 
-document.body.innerHTML = `
-    <svg style="width: 100%; height: calc(100vh - 20px)">
-      <g id="plot" />
-    </svg>`;
 const svg = document.querySelector('svg')!;
 const plot = svg.querySelector('#plot') as SVGElement;
 
-const hyperbola = (x: number, y: number) => x * y > 10000;
-
-const width = svg.clientWidth;
-const height = svg.clientHeight;
 const domain = {
-  x: -width / 2,
-  y: -height / 2,
-  width,
-  height
+  x: -svg.clientWidth / 2,
+  y: -svg.clientHeight / 2,
+  width: svg.clientWidth,
+  height: svg.clientHeight
 };
 plot.style.transform = `translate(${- domain.x}px, ${- domain.y}px)`;
 
-plot.append(...squaresToSvg(
-    new Plot<boolean>(hyperbola)
-        .compute(domain, /* sampleSpacing= */ 128, /* pixelSize= */ 1)
-        .squares(),
-    /* addStyles= */ (isInside, el) => {
-      el.style.stroke = isInside ? 'olive' : 'lightgreen';
-    }));
+const hyperbola = (x: number, y: number) => x * y > 10000;
+const runs = new Plot(hyperbola).compute(domain, 128, 1).runs();
+
+plot.append(...runsToSvg(runs, (el, isInside) => {
+  el.style.stroke = isInside ? 'olive' : 'lightgreen';
+}));
