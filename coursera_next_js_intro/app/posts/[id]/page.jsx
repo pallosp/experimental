2297cd4947c0a1dfbffd205395cf380bb4efa50e
Péter Pallos <pallosp@gmail.com>
2025-03-13 11:35:29 +0100

    Next.js data fetching

--- coursera_next_js_intro/app/posts/[id]/page.jsx
+++ coursera_next_js_intro/app/posts/[id]/page.jsx
@@ -1,8 +1,17 @@
+import { posts } from "../../data/posts";
+
 export default async function Posts({ params }) {
   const { id } = await params;
+
+  const post = posts.find((p) => p.id === id);
+  if (!post) {
+    return <h1>Post not found</h1>;
+  }
+
   return (
     <main>
-      <h1>Post ID: {id}</h1>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </main>
   );
 }
