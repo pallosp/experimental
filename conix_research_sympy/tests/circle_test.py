1ffba86684d4a56c17606f8c38f78f097f622430
Péter Pallos <pallosp@gmail.com>
2025-04-05 13:52:22 +0200

    The conic research project has been moved to its own repo

--- conix_research_sympy/tests/circle_test.py
+++
@@ -1,9 +0,0 @@
-from sympy import Matrix
-
-from lib.matrix import IsScalarMultiple
-from lib.circle import Circle
-
-
-def test_circle():
-    circle = Circle(0, 0, 1)
-    assert IsScalarMultiple(circle, Matrix.diag([1, 1, -1]))
